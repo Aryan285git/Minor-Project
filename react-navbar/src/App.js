bf096008e567710b274a5ca0a9c993db7d5e13ed
Harsh Vardhan Singh <harshvv839@gmail.com>
2023-11-29 01:20:08 +0530

    commit

--- react-navbar/src/App.js
+++ react-navbar/src/App.js
@@ -1,4 +1,3 @@
-// src/App.js
 import React, { useState, useEffect } from 'react';
 import NavBar from './NavBar';
 import axios from 'axios';
@@ -32,41 +31,24 @@ function App() {
     }
   };
 
-  const handlePredictNifty = async () => {
-    // Validate toDateNifty
-    if (!toDateNifty) {
-      alert('Please enter a valid To Date for Nifty.');
+  const handlePredict = async (symbol) => {
+    const toDate = symbol === 'nifty' ? toDateNifty : toDateSensex;
+
+    // Validate toDate
+    if (!toDate) {
+      alert(`Please enter a valid To Date for ${symbol === 'nifty' ? 'Nifty' : 'Sensex'}.`);
       return;
     }
 
     try {
-      // Make a POST request to the backend with toDateNifty
-      const response = await axios.post('http://localhost:3000/api/predictor', {
-        toDate: toDateNifty,
+      // Make a POST request to the backend with toDate
+      const response = await axios.post(`http://localhost:3000/api/predictor/${symbol}`, {
+        toDate,
       });
 
-      console.log('Predictor Response (Nifty):', response.data);
+      console.log(`Predictor Response (${symbol}):`, response.data);
     } catch (error) {
-      console.error('Error predicting Nifty:', error);
-    }
-  };
-
-  const handlePredictSensex = async () => {
-    // Validate toDateSensex
-    if (!toDateSensex) {
-      alert('Please enter a valid To Date for Sensex.');
-      return;
-    }
-
-    try {
-      // Make a POST request to the backend with toDateSensex
-      const response = await axios.post('http://localhost:3000/api/predictor', {
-        toDate: toDateSensex,
-      });
-
-      console.log('Predictor Response (Sensex):', response.data);
-    } catch (error) {
-      console.error('Error predicting Sensex:', error);
+      console.error(`Error predicting ${symbol}:`, error);
     }
   };
 
@@ -86,7 +68,7 @@ function App() {
             value={toDateNifty}
             onChange={(e) => setToDateNifty(e.target.value)}
           />
-          <button onClick={handlePredictNifty}>Submit</button>
+          <button onClick={() => handlePredict('nifty')}>Submit</button>
         </div>
         <div className="column">
           <h2>Sensex Predictor</h2>
@@ -96,7 +78,7 @@ function App() {
             value={toDateSensex}
             onChange={(e) => setToDateSensex(e.target.value)}
           />
-          <button onClick={handlePredictSensex}>Submit</button>
+          <button onClick={() => handlePredict('sensex')}>Submit</button>
         </div>
         <div className="column">
           <h2>Current Values</h2>
